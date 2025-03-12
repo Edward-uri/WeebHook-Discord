dfae67e3a4ade591184f3a206c6317c50e672a4a
Maax9o9 <maxrockogenial@gmail.com>
2025-03-11 19:52:20 -0600

    Add main dependencies routes

--- infraestructure/routes/payloadRoutes.go
+++ infraestructure/routes/payloadRoutes.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+    "weebhook/infraestructure/controller"
+    "github.com/gin-gonic/gin"
+)
+
+func Routes(router *gin.Engine, webhookHandler *controller.WebhookHandler) {
+    routes := router.Group("pull_request")
+    {
+        routes.POST("/webhook", webhookHandler.HandlePullRequest)
+    }
+}
--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+    "weebhook/infraestructure"
+    "weebhook/infraestructure/routes"
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    webhookHandler := infraestructure.Init()
+
+    router := gin.Default()
+
+    routes.Routes(router, webhookHandler)
+
+    router.Run(":8080")
+}
